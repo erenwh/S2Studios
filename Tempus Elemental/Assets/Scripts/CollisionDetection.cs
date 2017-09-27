f4f600aa077831bf0f1f27ef6344343111dd6158
Elijah Hauber <ehauber@purdue.edu>
2017-09-26 23:21:25 -0400

    Collision detection
    Simple draft code for collision detection.
    
    Not a complete draft because:
    
    http://answers.unity3d.com as of 09/26/17 is down for everyone via http://downforeveryoneorjustme.com/answers.unity3d.com

---
+++ Tempus Elemental/Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    public float forcePush;
+
+    private void OnCollisionEnter2D(Collision2D coll2d) // this part is reusable to detect collision
+    {
+        if (coll2d.gameObject.tag == "Walls") // push back after reaching wall
+        {
+            Rigidbody2D rb2d = coll2d.gameObject.GetComponent<Rigidbody2D>();
+            float x = rb2d.position.x;
+            float y = rb2d.position.y;
+
+            rb2d.angularVelocity = - x * 100;
+            rb2d.velocity = - transform.up * y * 100;
+        }
+    }
+}
