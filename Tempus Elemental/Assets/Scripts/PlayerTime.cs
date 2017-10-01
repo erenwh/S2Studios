879f5fe709ccf97ec17ef5787735144315eadb14
Michael Parr <MichaelParr@ParrTayGaming.com>
2017-10-01 15:05:29 -0400

    PlayerTime
    I created a PlayerTime class, which will call the relevant Player's
    death animation & let the game controller know as soon as they are
    implemented. timeRemaining variable is public so that it can be
    manipulated by other classes.

--- Tempus Elemental/Assets/Scripts/PlayerTime.cs
+++ Tempus Elemental/Assets/Scripts/PlayerTime.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerTime : MonoBehaviour {
 
-	public int time = 30;
+	public int startingTime = 30;
+	public int timeRemaining;
 
 	// Use this for initialization
 	void Start () {
-		
+
+		//Assuming player's begin immediately after spawning
+		BeginCountdown();
 	}
 	
 	// Update is called once per frame
@@ -17,9 +20,26 @@ public class PlayerTime : MonoBehaviour {
 	}
 
 	/// <summary>
-	/// Has the player's time start its 1 per second decrement. 
+	/// Has the player's time start its 1 per second decrement, and resets the initial time to whatever it should start with. 
 	/// </summary>
 	void BeginCountdown () {
+		timeRemaining = startingTime;
+		StartCoroutine("DecrementTime");
+	}
 
+	/// <summary>
+	/// Decrements the timeRemaining by 1 every second, calls relevant death methods upon going under 1 second of timeRemaining.
+	/// </summary>
+	/// <returns>The time.</returns>
+	IEnumerator DecrementTime () {
+		while (true) {
+			yield return new WaitForSeconds (1f);
+			timeRemaining--;
+			if (timeRemaining < 1) {
+				//Call Death animation & play sfx
+				//let game controller know
+				//Destroy (gameObject);
+			}
+		}
 	}
 }
