2c7e2b6caf7580cf26a5d001bb39450fbafa2f65
HanWang <erenwh@gmail.com>
2017-10-05 17:04:23 -0400

    changed push to charge ranged attack

--- Tempus Elemental/Assets/Scripts/Player/RangedAttack/RangedAttack.cs
+++ Tempus Elemental/Assets/Scripts/Player/RangedAttack/RangedAttack.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class RangedAttack : MonoBehaviour {
 	public float ProjectileForce;                           // the speed of the projectile
-    private int projectileHealth;              // the distance of the projectile can travel
     public GameObject fireball;
 	public string playerNum;
+	public float delay = 1f;										// ranged attack delay
+	public float timepassed;
+	private bool waitToCharging = false;
 
+	IEnumerator DelayTime () {
+		waitToCharging = true;
+		while (timepassed <= delay) {
+			yield return new WaitForSeconds (0.1f);
+			timepassed += 0.1f;
+		}
+	}
     // Use this for initialization
     void Start () {
-        
+		timepassed = 0f;
     }
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("FirePlayer" + playerNum)) {
-            GameObject newFireball = Instantiate(fireball, transform.position, transform.rotation);
-			newFireball.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0f, -ProjectileForce));
-        }
-	}
+		if (Input.GetButtonDown ("Fire" + gameObject.tag)) {
+			if (!waitToCharging) {
+				// wait for delay
+				timepassed = 0f;
+				StartCoroutine ("DelayTime");
+			} else {
+				
+			}
 
+        } else
+		if (timepassed >= delay && Input.GetButtonUp ("Fire" + gameObject.tag)) {
+			fire ();
+			waitToCharging = false;
+			timepassed = 0f;
+		}
+
+	}
+	void fire() {
+		GameObject newFireball = Instantiate(fireball, transform.position, transform.rotation);
+		newFireball.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector2 (0f, -ProjectileForce));
+	
+	}
 }
