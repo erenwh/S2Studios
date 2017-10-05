282f83e6da4ffee5d2f775772dfe5d7d13bd7547
Han Wang <erenwh@gmail.com>
2017-10-05 03:09:01 -0400

    Created Projectile artwork, Created Projectile prefab, Implemented Shooting machenism using F
    Created Projectile artwork, Created Projectile prefab, Implemented Shooting machenism using F

---
+++ Tempus Elemental/Assets/Scripts/Player/RangedAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAttack : MonoBehaviour {
+    public Vector2 speed;                           // the speed of the projectile
+    private int projectileHealth = 10;              // the distance of the projectile can travel
+    public GameObject fireball;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("FirePlayer1")) {
+            GameObject newFireball = Instantiate(fireball, transform.position, transform.rotation);
+            newFireball.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0f, 100f));
+        }
+	}
+
+}
