33a53f6e545c6698e0a8467aae8f06a43a159f22
ethauber <ehauber@purdue.edu>
2017-10-05 17:39:35 -0400

    Player Melee Attack
    Kills Both players when touching

--- Tempus Elemental/Assets/Scripts/Player/PlayerMeleeAttack.cs
+++ Tempus Elemental/Assets/Scripts/Player/PlayerMeleeAttack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMeleeAttack : MonoBehaviour {
 
-    public int damage = 2; //damage default to remove 2 seconds of time from player
+    public int damage = 5; //damage default to remove 2 seconds of time from player
     private int thisTime;
 
 	// Use this for initialization
@@ -14,6 +14,20 @@ public class PlayerMeleeAttack : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetButtonDown("Fire" + gameObject.tag))
+        {
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        // controller.AssignPlayer(coll.gameObject);
+        // controller.ActivatePowerup(ptype, powerupLength, speedMultiplier, amountTimeAdd);
+        if (coll.CompareTag("Player1") || coll.CompareTag("Player2") || coll.CompareTag("Player3") || coll.CompareTag("Player4"))
+        {
+            coll.gameObject.GetComponent<PlayerTime>().timeRemaining -= damage;
+            //gameObject.GetComponent<PlayerTime>().timeRemaining += damage;
+        }
+    }
 }
