f9cd67fac2d769200021df2a10a58ba16e498b2f
ethauber <ehauber@purdue.edu>
2017-10-05 18:19:59 -0400

    Player Attack
    Changed circular collider for attacking.
    It will move dynamically based on where the player is attack and moving.

--- Tempus Elemental/Assets/Scripts/Player/PlayerMovement.cs
+++ Tempus Elemental/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,13 +7,16 @@ public class PlayerMovement : MonoBehaviour {
     public float speed;					//how quickly the player moves
 	public float rotationSpeed = 30.0f;	//how quickly the player faces the direction they are moving
 	public bool charging = false;
+    public float circleOffsetCoefficient = .18f;
 
+    private CircleCollider2D cc2d;
     private Rigidbody2D rb2d;
 
     void Start()
     {
 		charging = false;
         rb2d = GetComponent<Rigidbody2D>();
+        cc2d = GetComponent<CircleCollider2D>();
     }
 		
     private void FixedUpdate()
@@ -27,7 +30,7 @@ public class PlayerMovement : MonoBehaviour {
 			Vector2 movement = new Vector2 (moveHorizontal, moveVertical).normalized;
 
 			rb2d.velocity = movement * speed;
-
+            cc2d.offset = movement * circleOffsetCoefficient;
 			//(Parr's rotation suggestion) <- delete this comment if you like it :)
 			//Angular Movement
 //		if (Mathf.Abs (moveHorizontal) > 0.1 || Mathf.Abs (moveVertical) > 0.1) {
