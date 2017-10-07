e624a39dff7199b9fddff0baac0840cf3920c22d
LOG67 <m.einakchi@gmail.com>
2017-10-07 16:27:47 -0500

    Added Utils for static utils functions, refactored some of the scripts to use utils for getting the movement of the player

--- Tempus Elemental/Assets/Scripts/Player/PlayerMovement.cs
+++ Tempus Elemental/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,24 +1,22 @@
-﻿
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float speed;					//how quickly the player moves
-	public float rotationSpeed = 30.0f;	//how quickly the player faces the direction they are moving
+    public float speed = 3f;					//how quickly the player moves
 	public bool charging = false;
-	public float circleOffsetCoefficient = .18f;
-	public Vector2 lastDirection;
+	// this is for the circular collider for attack
+	public float circleOffsetCoefficient = .18f; 
 
 	private CircleCollider2D cc2d;
 	private Rigidbody2D rb2d;
+    private Vector2 lastDirection;
 
 	void Start()
 	{
 		charging = false;
 		rb2d = GetComponent<Rigidbody2D>();
 		cc2d = GetComponent<CircleCollider2D>();
+
 	}
 
 	public Vector2 FacingDirection() {
@@ -27,29 +25,21 @@ public class PlayerMovement : MonoBehaviour {
 
 	private void FixedUpdate()
 	{
-		if (!charging) {
-			//x
-			float moveHorizontal = Input.GetAxis ("Horizontal" + gameObject.tag);
-			//y
-			float moveVertical = Input.GetAxis ("Vertical" + gameObject.tag);
+        if (charging) 
+        {
+            rb2d.velocity = Vector2.zero;
+        } 
+        else 
+        {
+            Vector2 movement = Utils.GetPlayerMovement(tag);
 
-			Vector2 movement = new Vector2 (moveHorizontal, moveVertical).normalized;
-
-			if (Mathf.Abs (movement.x) > Mathf.Epsilon || Mathf.Abs (movement.y) > Mathf.Epsilon) 
+            if (Utils.IsPlayerMoving(tag)) 
 			{
 				lastDirection = movement;
 			}
 
 			rb2d.velocity = movement * speed;
-			cc2d.offset = lastDirection * circleOffsetCoefficient;
-			//(Parr's rotation suggestion) <- delete this comment if you like it 🙂
-			//Angular Movement
-			//		if (Mathf.Abs (moveHorizontal) > 0.1 || Mathf.Abs (moveVertical) > 0.1) {
-			//			float angle = Mathf.Atan2 (moveVertical, moveHorizontal) * Mathf.Rad2Deg - 90;
-			//			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.AngleAxis (angle, Vector3.forward), Time.deltaTime * rotationSpeed);
-			//		} 
-		} else {
-			rb2d.velocity = Vector2.zero;
-		}
+			cc2d.offset = lastDirection * circleOffsetCoefficient;			
+		} 
 	}
 }
--- Tempus Elemental/Assets/Scripts/Player/PlayerRangedAttack.cs
+++ Tempus Elemental/Assets/Scripts/Player/PlayerRangedAttack.cs
@@ -1,53 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class RangedAttack : MonoBehaviour {
-	//public float ProjectileForce = 150f;                  // the speed of the projectile
+public class PlayerRangedAttack : MonoBehaviour {
+    
     public GameObject fireball;
-	//public string playerNum;
 	public float delay = 0.5f;								// ranged attack delay
 	public int costToThrow = 1;								// how much time does it take to throw an attack
-	public float timepassed;
+	private float timepassed;
 	private bool waitToCharging = false;
-	public Vector2 aimDirc;
+	private Vector2 aimDirc;
 	private PlayerMovement pm;
 
-//	IEnumerator DelayTime () {
-//		while (timepassed <= delay) {
-//			yield return new WaitForSeconds (0.1f);
-//			timepassed += 0.1f;
-//		}
-//	}
 
     // Use this for initialization
-    void Start () {
+    void Start() 
+    {
 		timepassed = 0f;
-		pm = GetComponent<PlayerMovement> ();
+		pm = GetComponent<PlayerMovement>();
     }
 
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetButton ("Fire" + gameObject.tag)) {
-			if (!waitToCharging) {
+	void Update() 
+    {
+		if (Input.GetButton ("Fire" + gameObject.tag)) 
+        {
+			if (!waitToCharging) 
+            {
 				// wait for delay
 				timepassed = 0f;
 				waitToCharging = true;
 				//StartCoroutine ("DelayTime");
 				pm.charging = true;
 			} 
-            else {
+            else 
+            {
                 // change direction while aiming, player cannot move
-				aimDirc = new Vector2(Input.GetAxis("Horizontal" + gameObject.tag), Input.GetAxis("Vertical" + gameObject.tag)).normalized;
-				if (Mathf.Abs (aimDirc.x) < Mathf.Epsilon && Mathf.Abs (aimDirc.y) < Mathf.Epsilon) {
+                aimDirc = Utils.GetPlayerMovement(tag);
+                if (!Utils.IsPlayerMoving(tag)) 
+                {
 					aimDirc = pm.FacingDirection ();
-				} else {
-					pm.lastDirection = aimDirc;
-				}
+				} 
 			}
-		} else if (Input.GetButtonUp ("Fire" + gameObject.tag)) {
+		} 
+        else if (Input.GetButtonUp ("Fire" + gameObject.tag)) 
+        {
 			if (timepassed >= delay) {
-            	fire();
+            	Fire();
 			}
             waitToCharging = false;
 			timepassed = 0f;
@@ -55,12 +52,14 @@ public class RangedAttack : MonoBehaviour {
 		}
 
 		//DelayTime
-		if (waitToCharging) {
+		if (waitToCharging) 
+        {
 			timepassed += Time.deltaTime;
 		}
 	}
 
-	void fire() {
+	void Fire() 
+    {
 		gameObject.GetComponent<PlayerTime>().DecrementTime(costToThrow);
 		GameObject newFireball = Instantiate(fireball, transform.position, Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.down, aimDirc)));
 		newFireball.GetComponent<ProjectileScript> ().setAim(aimDirc);
