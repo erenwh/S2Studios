e6911ff82da0179fec758f1c747a783de691b43e
Michael Parr <MichaelParr@ParrTayGaming.com>
2017-10-19 21:35:37 -0400

    Game Menu Setup
    "Hi Eli" incorporated

---
+++ Tempus Elemental/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void onGameSetup() {
+		Debug.Log ("Hi Eli");
+	}
+}
