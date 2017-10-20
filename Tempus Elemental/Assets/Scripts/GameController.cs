18f765564a9cdbe7d1321cd11a36bcfdf936624c
Michael Parr <MichaelParr@ParrTayGaming.com>
2017-10-19 21:46:49 -0400

    GameController Initialized
    We created the Game Controller class and figured out a solution for
    transferring its data between scenes.

---
+++ Tempus Elemental/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	//constants
+
+	//references
+
+	//variables
+
+	// This is the permanent source of information for our game modes
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
