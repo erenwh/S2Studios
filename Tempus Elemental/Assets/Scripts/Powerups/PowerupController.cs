87c7d5ba9db7f1b2bec2df4b8007815ed979c7e3
rhbrian <brhee@purdue.edu>
2017-10-23 18:40:12 -0400

    converted previous implementations of powerup to change distortion types when collided with
    freeze, speedup, normal all implemented

--- Tempus Elemental/Assets/Scripts/Powerups/PowerupController.cs
+++ Tempus Elemental/Assets/Scripts/Powerups/PowerupController.cs
@@ -3,7 +3,7 @@
 public class PowerupController : MonoBehaviour 
 {
     private GameObject touchedPlayer;       // keep track of the player who touched it
-    private int ptype;                      // powerup type | 0: add time, 1: speed up player
+    private int ptype;                      // powerup type | 0: add time, 1: speed up player, 2: freeze time, 3: slow down time
     private bool powerupActive;             // powerup is active
     private float speedMultiplier;          // movement speed multiplier
     private float ogspeed;
@@ -17,21 +17,42 @@ public class PowerupController : MonoBehaviour
         {
             if (flag == 0)
             {
-                touchedPlayer.GetComponent<PlayerMovement>().speed += speedMultiplier;  // speed up
-                flag = 1;
+                if (ptype == 1) // have to change this so that later it changes the distortion instead of the powerup changing the properties
+                {
+                    // touchedPlayer.GetComponent<PlayerMovement>().speed += speedMultiplier;  // speed up
+                    touchedPlayer.GetComponent<DistortionCreator>().distortionType = 1;
+                    flag = 1;
+                }
+                else if (ptype == 2) // have to change this so that later it changes the distortion instead of the powerup changing the properties
+                {   // freeze time
+                    touchedPlayer.GetComponent<DistortionCreator>().distortionType = 2;
+                    flag = 1;
+                }
+                else if (ptype == 0) // have to change this so that later it changes the distortion instead of the powerup changing the properties
+                {   // slow down time
+                    touchedPlayer.GetComponent<DistortionCreator>().distortionType = 0;
+                    flag = 1;
+                }
             }
-            powerupLengthCounter -= Time.deltaTime;                                 // only run for that length
+            /*powerupLengthCounter -= Time.deltaTime;                                 // only run for that length
             
             if (powerupLengthCounter <= 0)               
             {
                 if (ptype == 1)                                                     // if powerup type is speed up
                 {
-
                     touchedPlayer.GetComponent<PlayerMovement>().speed = ogspeed;   // return to original speed
+                }
+                else if (ptype == 2)
+                {
+
+                }
+                else if (ptype == 3)
+                {
+
                 }
                 powerupActive = false;
                 flag = 0;
-            }
+            }*/
         }
 
     }
@@ -48,13 +69,15 @@ public class PowerupController : MonoBehaviour
         speedMultiplier = multiplier;
         ogspeed = touchedPlayer.GetComponent<PlayerMovement>().speed;               // keep track of speed of player
 
-        if (ptype == 0) // if powerup type is add time 
+        powerupActive = true;
+
+        /*if (ptype == 0) // if powerup type is add time 
         {
 			touchedPlayer.GetComponent<PlayerTime>().AddTime(time2add);           // add time
         }
         else            // if different powerup type
         {
             powerupActive = true; 
-        }
+        }*/
     }
 }
