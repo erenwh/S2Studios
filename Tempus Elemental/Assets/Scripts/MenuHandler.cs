57e825d036914aae30f2aef15dc5cb1ea681325b
Michael Parr <MichaelParr@ParrTayGaming.com>
2017-10-30 23:54:19 -0400

    Game Mode implementation continued
    Worked on more menu handler implementation

--- Tempus Elemental/Assets/Scripts/MenuHandler.cs
+++ Tempus Elemental/Assets/Scripts/MenuHandler.cs
@@ -5,8 +5,20 @@ using UnityEngine.UI;
 
 public class MenuHandler : MonoBehaviour
 {
+	//const
+	private const int FREEFORALL = 0;
+	private const int KINGOFTHEHILL = 1;
+	private const int TEAMDEATHMATCH = 2;
+
+	//references
     public Dropdown selectPlayerDropdown;
     public Text selectedAmountPlayers;
+	public Text selectedGameModeText;
+
+	//variables
+	public int numGameModes = 3;
+	private int selectedGameMode = 0;
+
 
     private List<string> numPlayers = 
         new List<string>() { 
@@ -21,12 +33,33 @@ public class MenuHandler : MonoBehaviour
 
 	//right arrow selecting the game mode
 	public void NextGameMode () {
-		
+		selectedGameMode++;
+		if (selectedGameMode >= numGameModes) {
+			selectedGameMode = 0;
+		}
 	}
 
 	//left arrow selecting the game mode
 	public void PrevGameMode () {
-		
+		selectedGameMode--;
+		if (selectedGameMode < 0) {
+			selectedGameMode = numGameModes - 1;
+		}
+	}
+
+	//sets the text in the menu so that the player knows
+	void SetGameModeText () {
+		switch (selectedGameMode) {
+		case FREEFORALL:
+			selectedGameModeText.text = "Free For All";
+			break;
+		case KINGOFTHEHILL:
+			selectedGameModeText.text = "King of the Hell";
+			break;
+		case TEAMDEATHMATCH:
+			selectedGameModeText.text = "Team Death Match";
+			break;
+		}
 	}
 
     public void SelectPlayer(int index)
