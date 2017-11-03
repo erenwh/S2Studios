e322115ff9b8226ba57b447a7a486d8683727415
ethauber <ehauber@purdue.edu>
2017-11-02 22:25:15 -0400

    Splash Screen now holds the Game Gameobject
    This should stop the crashing upon multiple games in a row.

---
+++ Tempus Elemental/Assets/Scripts/Splash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour {
+
+    public float splashTime = 2.0f;         //how long will the splash be shown
+    private float viewingTime = 0.0f;       //how long the scene has been viewed
+	
+	// Update is called once per frame
+	void Update () {
+        viewingTime += Time.deltaTime;
+        if (viewingTime >= splashTime) {
+            SceneManager.LoadScene("Menu");
+        }
+	}
+}
