e3d7398087d555e2a62bf0f1cc0dd90b03a95ddb
LOG67 <m.einakchi@gmail.com>
2017-11-03 16:51:43 -0400

    Added logic of the KOTHController

--- Tempus Elemental/Assets/Scripts/GameControllers/KOTHController.cs
+++ Tempus Elemental/Assets/Scripts/GameControllers/KOTHController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class KOTHController : GameController 
 {
-    
+    public float stealTreshold = 1.0f;
+
+    private List<GameObject> playersInTheZone = new List<GameObject>();
+
 	protected override bool VictoryCondition () 
     {
-		return false;
+		if (players.Count <= 1)
+		{
+			return true;
+		}
+
+		// this means no end-game state has been breached and the game continues
+		return false; 
 	}
 
     protected override void GameLogic() 
     {
-        
+                
+    }
+
+    public void PlayerEnteredZone(GameObject player) 
+    {
+        playersInTheZone.Remove(player);
+    }
+
+    public void PlayerLeftZone(GameObject player) 
+    {
+        playersInTheZone.Add(player);
     }
 
 	protected override string VictoryText () 
