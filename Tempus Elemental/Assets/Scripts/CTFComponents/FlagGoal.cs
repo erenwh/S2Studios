d1f8bf6fcd536d2fbe7b2b68c6a5f5a32f08f53d
rhbrian <brianhrhee@gmail.com>
2017-11-27 21:42:06 -0500

    created flagGoal script

---
+++ Tempus Elemental/Assets/Scripts/CTFComponents/FlagGoal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagGoal : MonoBehaviour {
+	public int goalTeam;
+
+	void OnTriggerEnter2D (Collider2D coll) {
+		if (coll.tag == "Player2" || coll.tag == "Player4" || coll.tag == "Player1" || coll.tag == "Player3") {
+			if (coll.GetComponent<PlayerFlags> ().hasFlag == true) {
+				if (goalTeam == 0) {
+					if (coll.tag == "Player1" || coll.tag == "Player3") {
+						coll.GetComponent<PlayerFlags> ().hasFlag = false;
+						coll.GetComponent<PlayerFlags> ().numFlags++;
+						coll.gameObject.GetComponent<PlayerMovement> ().speed = coll.gameObject.GetComponent<PlayerMovement> ().speed * 2;
+						//Game.Instance.GetComponent<FlagSpawner> ().respawn = 1;
+						FindObjectOfType<FlagSpawner> ().respawn = 1;
+						//GetComponent<FlagSpawner> ().respawn = 1;
+					}
+				} else if (goalTeam == 1) {
+					if (coll.tag == "Player2" || coll.tag == "Player4") {
+						coll.GetComponent<PlayerFlags> ().hasFlag = false;
+						coll.GetComponent<PlayerFlags> ().numFlags++;
+						coll.gameObject.GetComponent<PlayerMovement> ().speed = coll.gameObject.GetComponent<PlayerMovement> ().speed * 2;
+						FindObjectOfType<FlagSpawner> ().respawn = 2;
+					}
+				}
+			}
+		}
+	}
+}
