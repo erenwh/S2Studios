3c08a283d04018d9d5894d25e542bc1e62a63eea
rhbrian <brianhrhee@gmail.com>
2017-11-27 21:41:58 -0500

    created flag script

---
+++ Tempus Elemental/Assets/Scripts/CTFComponents/Flag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour {
+	public int flagTeam;
+
+	void OnTriggerEnter2D (Collider2D coll) {
+		if (coll.tag == "Player2" || coll.tag == "Player4" || coll.tag == "Player1" || coll.tag == "Player3") {
+			if (flagTeam == 0) {
+				if (coll.tag == "Player2" || coll.tag == "Player4") {
+					coll.GetComponent<PlayerFlags> ().hasFlag = true;
+					coll.GetComponent<PlayerFlags> ().holdingFlag (coll.gameObject);
+					Destroy (gameObject);
+				}
+			} else if (flagTeam == 1) {
+				if (coll.tag == "Player3" || coll.tag == "Player1") {
+					coll.GetComponent<PlayerFlags> ().hasFlag = true;
+					coll.GetComponent<PlayerFlags> ().holdingFlag (coll.gameObject);
+					Destroy (gameObject);
+				}
+			}
+		}
+		//Destroy (gameObject);
+	}
+}
