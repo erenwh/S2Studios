01fac3cabe663da8e4f9cdc2ad1ee5b58f978c02
Michael Parr <MichaelParr@ParrTayGaming.com>
2017-12-04 17:30:48 -0500

    Minor change, still working on getting final build
    It looks like the tests are getting in the way of the build. I'll try
    removing them for the build

--- Tempus Elemental/Assets/Scripts/CTFComponents/Flag.cs
+++ Tempus Elemental/Assets/Scripts/CTFComponents/Flag.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Flag : MonoBehaviour {
 	public int flagTeam;
 	public Vector3 altPosition;			//used on maps 3 and 4
+	private FlagSpawner fs;
 
 	void Start () {
-		transform.position = altPosition;
+		if (Game.Instance.mapSelected > 1) {
+			transform.position = altPosition;
+		}
+		fs = GameObject.FindGameObjectWithTag ("FlagSpawner").GetComponent<FlagSpawner>();
 	}
 
 	void OnTriggerEnter2D (Collider2D coll) {
@@ -17,12 +21,18 @@ public class Flag : MonoBehaviour {
 					coll.GetComponent<PlayerFlags> ().hasFlag = true;
 					coll.GetComponent<PlayerFlags> ().holdingFlag (coll.gameObject);
 					Destroy (gameObject);
+				} else if (coll.tag == "Player1" || coll.tag == "Player3") {
+					Destroy (gameObject);
+					fs.respawn = 1;
 				}
 			} else if (flagTeam == 1) {
 				if (coll.tag == "Player3" || coll.tag == "Player1") {
 					coll.GetComponent<PlayerFlags> ().hasFlag = true;
 					coll.GetComponent<PlayerFlags> ().holdingFlag (coll.gameObject);
 					Destroy (gameObject);
+				} else if (coll.tag == "Player2" || coll.tag == "Player4") {
+					Destroy (gameObject);
+					fs.respawn = 2;
 				}
 			}
 		}
